b292d0270bb9354638513c421a9d9dd97a106622
LucaCalveiro <133456804+JKiller3766@users.noreply.github.com>
2025-05-15 12:23:45 +0200

    ex2

--- 2025_PRAC3/src/pckg/Exer_02_ChangeToBinary.java
+++ 2025_PRAC3/src/pckg/Exer_02_ChangeToBinary.java
@@ -7,6 +7,17 @@ public class Exer_02_ChangeToBinary {
 
 		/* COMPLETE */
 		//Perform some tests here
+		System.out.println("33 = " + toBinaryString(33));
+		System.out.println("278 = " + toBinaryString(278));
+		System.out.println("225 = " + toBinaryString(225));
+		System.out.println("545 = " + toBinaryString(545));
+		System.out.println("1214 = " + toBinaryString(1214));
+		System.out.println("44 = " + toBinaryString(44));
+		System.out.println("95 = " + toBinaryString(95));
+		System.out.println("0 = " + toBinaryString(0));
+		System.out.println("1 = " + toBinaryString(1));
+		System.out.println("78 = " + toBinaryString(78));
+		System.out.println("729 = " + toBinaryString(729));
 
 	}
 	
@@ -14,8 +25,15 @@ public class Exer_02_ChangeToBinary {
 	public static String toBinaryString(int n) {
 		/* COMPLETE */
 		// Write the recursive function that creates the binary representation of n (n>=0)
-
-		return null; //Change this to return the corresponding String
+		if (n == 0) {
+			return "0";
+		} else if (n /2 == 0) {
+			return "" + n%2;
+		}
+		else {
+			String binary = "" + n%2;
+			return toBinaryString((n/2)) + binary;
+		}
 	}
 	
 	
